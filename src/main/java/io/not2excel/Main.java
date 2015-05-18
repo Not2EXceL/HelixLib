7f8eced8fa4d40f530f19c93158b71e7e7430de7
Richmond Steele <not2excel@gmail.com>
2015-05-17 21:19:50 -1000

    Library basic setup complete

---
+++ src/main/java/io/not2excel/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2011-Current Richmond Steele (Not2EXceL) (nasm) <not2excel@gmail.com>
+ * 
+ * This file is part of helixlib.
+ * 
+ * helixlib can not be copied and/or distributed without the express
+ * permission of the aforementioned owner.
+ */
+package io.not2excel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
