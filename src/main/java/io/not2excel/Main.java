c92f4691d1bff6fe4f5bea1adec4d54c621fae47
Richmond Steele <not2excel@gmail.com>
2015-05-17 21:27:59 -1000

    Migrated eventapi and moduleapi into the lib.  ClassEnumerator was migrated as well

--- src/main/java/io/not2excel/Main.java
+++ src/main/java/io/not2excel/Main.java
@@ -1,16 +1,25 @@
 /*
  * Copyright (C) 2011-Current Richmond Steele (Not2EXceL) (nasm) <not2excel@gmail.com>
  * 
- * This file is part of helixlib.
+ * This file is part of module-api.
  * 
- * helixlib can not be copied and/or distributed without the express
+ * module-api can not be copied and/or distributed without the express
  * permission of the aforementioned owner.
  */
 package io.not2excel;
 
+import io.not2excel.module.loader.ModuleLoader;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] main) {
+        ModuleLoader<?> loader = () -> null;
+        loader.getInternalModules().forEach((s, l) -> {
+            System.out.println("<====================>");
+            System.out.println(s);
+            System.out.println("----------------------");
+            System.out.println(l.toString());
+            System.out.println("<====================>");
+        });
     }
 }
