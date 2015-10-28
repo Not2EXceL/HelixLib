971d4209ca44c081baa9e3eae793d1101cf850d7
Carter Gale <buildfresh@gmail.com>
2015-10-27 18:32:01 -0700

    Added a weighted randomized collection

---
+++ core/src/main/java/io/not2excel/collection/WeightedRandomCollection.java
@@ -0,0 +1,35 @@
+package io.not2excel.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by carter on 5/15/2015.
+ */
+public class WeightedRandomCollection<E> {
+    private final NavigableMap<Double, E> map = new TreeMap<>();
+    private final ThreadLocalRandom random;
+    private double total = 0;
+
+    public WeightedRandomCollection() {
+        this.random = ThreadLocalRandom.current();
+    }
+
+    public void add(double weight, E result) {
+        if (weight <= 0) return;
+        total += weight;
+        map.put(total, result);
+    }
+
+    public List<E> values(){
+        return new ArrayList<>(map.values());
+    }
+
+    public E next() {
+        double value = random.nextDouble() * total;
+        return map.ceilingEntry(value).getValue();
+    }
+}
